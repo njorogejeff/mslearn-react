9954f2e06ca4e78bd3ec02fbd3d8f5dc9e000f9e
Njoroge Jeff <njorogejeff254@gmail.com>
2023-12-28 16:47:14 +0300

    Add React and import map files, create RecipeTitle component, and render App component

---
+++ code/0-starter/build/dist/App.js
@@ -0,0 +1,7 @@
+import "./index.css.proxy.js";
+import React from "../_snowpack/pkg/react.js";
+import RecipeTitle from "./RecipeTitle.js";
+function App() {
+  return /* @__PURE__ */ React.createElement("article", null, /* @__PURE__ */ React.createElement("h1", null, "Recipe Manager"), /* @__PURE__ */ React.createElement(RecipeTitle, null));
+}
+export default App;
---
+++ code/0-starter/build/dist/RecipeTitle.js
@@ -0,0 +1,7 @@
+import React from "../_snowpack/pkg/react.js";
+function RecipeTitle() {
+  const title = "Mashed potatoes";
+  return /* @__PURE__ */ React.createElement("h2", null, title);
+}
+;
+export default RecipeTitle;
